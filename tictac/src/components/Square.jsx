80b2e88fd97b9cc5553d85b0342d857eef882448
Shashank-saurabh123 <saurabh123super@gmail.com>
2024-06-11 16:49:24 +0530

    done with tic tac toe

--- tictac/src/components/Square.jsx
+++ tictac/src/components/Square.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Square() {
+function Square(props) {
   return (
-    <div>
-      
+    <div 
+    onClick={props.onClick}
+    style={{border:"1px solid",
+        height:"100px",width:"100%",
+        display:"flex",
+        justifyContent:"center",
+        alignItems:"center"
+    }}
+    className='square'>
+      <h5>{props.value}</h5>
     </div>
   )
 }
